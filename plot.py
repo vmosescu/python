5968337e7f005a1c9e581d1a2e9c310e988a05dc
vmosescu <vmosescu@hotmail.com>
2023-04-22 11:10:09 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -10,3 +10,27 @@ def plotImages(images_arr):
     
 images = [x_train[i] for i in range(5)]
 plotImages(images)
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
