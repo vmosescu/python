4232ed57ce829d9f18807c1a98c04f282358778b
vmosescu <vmosescu@hotmail.com>
2023-04-22 10:58:31 +0300

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
+    
+images = [x_train[i] for i in range(5)]
+plotImages(images)
